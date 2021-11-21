095d47409d4ce494bcbba9c715289a2cd6eb6e90
cracked-machine <chrisjsutton@gmail.com>
2021-11-21 22:14:27 +0000

    restructured and added catch2 tests

--- source/tests/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <tlc5955.hpp>
-#include <iostream>
-
-int main()
-{
-    tlc5955::Driver leds;
-    std::cout << leds.startup_tests() << std::endl;
-    return 0;
-}
---
+++ source/tests/test.cpp
@@ -0,0 +1,13 @@
+
+
+#define CATCH_CONFIG_MAIN
+#include "../../build/catch.hpp"
+#include <iostream>
+#include <tlc5955.hpp>
+
+TEST_CASE("Testing framework is working fine", "[Catch2]")
+{
+
+    REQUIRE(true);
+    
+}
