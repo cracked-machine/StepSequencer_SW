3a6e2815455ba3597ab248923579d1b28e9e13d8
cracked-machine <chrisjsutton@gmail.com>
2021-12-05 15:50:45 +0000

    added tlc5955 test code to mainapp

--- main_app/src/mainapp.cpp
+++ main_app/src/mainapp.cpp
@@ -33,28 +33,27 @@
 		// oled.write_string(text, small_font, ssd1306::Colour::White, 3);
 		// oled.update_screen();
 		
-		// std::bitset<tlc5955::Driver::m_bc_data_resolution> led_bc {127};
-		// std::bitset<tlc5955::Driver::m_mc_data_resolution> led_mc {4};
-		// std::bitset<tlc5955::Driver::m_dc_data_resolution> led_dc {127};
-		// std::bitset<tlc5955::Driver::m_gs_data_resolution> led_gs {32767};
-		// tlc5955::Driver leds;
-		
-		// leds.startup_tests();
-		
-		// leds.set_control_bit(true);
-		// leds.set_ctrl_cmd_bits();
-		// leds.set_padding_bits();
-		// leds.set_function_data(true, true, true, true, true);
+		std::bitset<tlc5955::Driver::m_bc_data_resolution> led_bc {127};
+		std::bitset<tlc5955::Driver::m_mc_data_resolution> led_mc {4};
+		std::bitset<tlc5955::Driver::m_dc_data_resolution> led_dc {127};
+		std::bitset<tlc5955::Driver::m_gs_data_resolution> led_gs {32767};
+		tlc5955::Driver leds;
 
-		// leds.set_bc_data(led_bc, led_bc, led_bc);
-		// leds.set_mc_data(led_mc, led_mc, led_mc);
-		// // leds.set_all_dc_data(led_dc, led_dc, led_dc);
-		// leds.send_data();
-		//leds.flush_common_register();
+		
+		leds.set_control_bit(true);
+		leds.set_ctrl_cmd_bits();
+		leds.set_padding_bits();
+		leds.set_function_data(true, true, true, true, true);
+
+		leds.set_bc_data(led_bc, led_bc, led_bc);
+		leds.set_mc_data(led_mc, led_mc, led_mc);
+		leds.set_all_dc_data(led_dc, led_dc, led_dc);
+		leds.send_data();
+		leds.flush_common_register();
 		
 		//leds.send_control_data();
 		uint8_t count = 0;
-
+		uint32_t delay_ms {0};
 		while(true)
 		{
 	
@@ -64,14 +63,14 @@
 			if (count < font.character_map.size() - 1) { count++; }
 			else { count = 0; }		
 
-			//leds.set_control_bit(false);
-			//leds.set_all_gs_data(led_gs, led_gs, led_gs);
-	//		leds.send_data();
-			//leds.flush_common_register();
+			leds.set_control_bit(false);
+			leds.set_all_gs_data(led_gs, led_gs, led_gs);
+			leds.send_data();
+			leds.flush_common_register();
 #ifdef USE_HAL_DRIVER
-			HAL_Delay(1000);
+			HAL_Delay(delay_ms);
 #else
-			std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+			std::this_thread::sleep_for(std::chrono::milliseconds(delay_ms));
 #endif
 			// leds.flush_common_register();
 			//HAL_Delay(1);
