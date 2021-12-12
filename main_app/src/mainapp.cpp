ed70a2a433596cfa6bc40610a2cf22277fd81692
cracked-machine <chrisjsutton@gmail.com>
2021-12-12 15:20:16 +0000

    modified example code to use new tcl5955 and bit_utils

--- main_app/src/mainapp.cpp
+++ main_app/src/mainapp.cpp
@@ -47,41 +47,15 @@ void HAL_SPI_TxCpltCallback(SPI_HandleTypeDef *hspi)
 void mainapp()
 {
 
-	std::bitset<tlc5955::Driver::m_latch_size_bits> ctrl_latch_cmd {0b1};
-	std::bitset<tlc5955::Driver::m_latch_size_bits> gs_latch_cmd {0b0};
-	std::bitset<tlc5955::Driver::m_ctrl_cmd_size_bits> ctrl_cmd {0x96};
-	std::bitset<tlc5955::Driver::m_padding_section_size_bits> padding {0x00};
-	std::bitset<tlc5955::Driver::m_func_data_section_size_bits> function_cmd {0b10111};
-	std::bitset<tlc5955::Driver::m_bc_data_resolution> led_bc {0b000'0001};	
-	std::bitset<tlc5955::Driver::m_bc_data_resolution> led_bc_on {0b111'1111};	
-	std::bitset<tlc5955::Driver::m_bc_data_resolution> led_bc_off {0b000'0000};	
-	std::bitset<tlc5955::Driver::m_mc_data_resolution> led_mc {0b100};		
-	std::bitset<tlc5955::Driver::m_dc_data_resolution> led_dc {0x7F};		
-	std::bitset<tlc5955::Driver::m_gs_data_resolution> led_gs {0x7FFF};
-
 	tlc5955::Driver leds;
-	leds.m_common_bit_register.reset();
-	leds.add_bitset(leds.m_common_bit_register, ctrl_latch_cmd, tlc5955::Driver::m_latch_offset);
-	leds.add_bitset(leds.m_common_bit_register, ctrl_cmd, tlc5955::Driver::m_ctrl_cmd_offset);
-	leds.add_bitset(leds.m_common_bit_register, padding, tlc5955::Driver::m_padding_offset);
-	leds.add_bitset(leds.m_common_bit_register, function_cmd, tlc5955::Driver::m_func_data_offset);
-	// BC data
-	leds.add_bitset(leds.m_common_bit_register, led_bc_on, tlc5955::Driver::m_bc_data_offset);
-	leds.add_bitset(leds.m_common_bit_register, led_bc_on, tlc5955::Driver::m_bc_data_offset + tlc5955::Driver::m_bc_data_resolution);
-	leds.add_bitset(leds.m_common_bit_register, led_bc_on, tlc5955::Driver::m_bc_data_offset + tlc5955::Driver::m_bc_data_resolution * 2);
-	// MC data
-	leds.add_bitset(leds.m_common_bit_register, led_mc, tlc5955::Driver::m_mc_data_offset);
-	leds.add_bitset(leds.m_common_bit_register, led_mc, tlc5955::Driver::m_mc_data_offset + tlc5955::Driver::m_mc_data_resolution);
-	leds.add_bitset(leds.m_common_bit_register, led_mc, tlc5955::Driver::m_mc_data_offset + tlc5955::Driver::m_mc_data_resolution * 2);	
-	// DC data
-	for (uint8_t dc_idx = 0; dc_idx < 48; dc_idx++)
-	{
-		leds.add_bitset(leds.m_common_bit_register, led_dc, tlc5955::Driver::m_dc_data_offset + tlc5955::Driver::m_dc_data_resolution * dc_idx);		
-	}
-
-	leds.bitset_to_bytearray( leds.m_common_byte_register, leds.m_common_bit_register);
-	leds.print_bits(leds.m_common_bit_register); 
-
+	leds.reset();
+	leds.set_latch_cmd(true);
+	leds.set_function_cmd(true, false, true, true, true);
+	leds.set_global_brightness_cmd(0x7F, 0x7F, 0x7F);
+	leds.set_max_current_cmd(0x4, 0x4, 0x4);
+	leds.set_dot_correction_cmd_all(0x7F);
+	leds.process_register();
+	
 	// enable the gsclk
 	#ifdef USE_HAL_DRIVER
     	HAL_GPIO_WritePin(TLC5955_SPI2_GSCLK_GPIO_Port, TLC5955_SPI2_GSCLK_Pin, GPIO_PIN_SET);
@@ -104,16 +78,11 @@ void mainapp()
 		if (count < font.character_map.size() - 1) { count++; }
 		else { count = 0; }		
 
-		// set led driver gs data
-		leds.m_common_bit_register.reset();
-		leds.add_bitset(leds.m_common_bit_register, gs_latch_cmd, tlc5955::Driver::m_latch_offset);
-		for (uint16_t gs_idx = 0; gs_idx < 48; gs_idx++)
-		{
-			leds.add_bitset(leds.m_common_bit_register, led_gs, tlc5955::Driver::m_gs_data_offset + tlc5955::Driver::m_gs_data_resolution * gs_idx);
-		}
-
-		leds.bitset_to_bytearray( leds.m_common_byte_register, leds.m_common_bit_register);
-		leds.print_bits(leds.m_common_bit_register); 
+		// // set led driver gs data
+		leds.reset();
+		leds.set_latch_cmd(false);
+		leds.set_greyscale_cmd_all(0x7FFF);
+		leds.process_register();
 		
 	}
 }
