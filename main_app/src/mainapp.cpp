9417aefed4cabaef03dd5f4024e22b4f2278427b
cracked-machine <chrisjsutton@gmail.com>
2022-01-04 18:31:30 +0000

    updated submodules

--- main_app/src/mainapp.cpp
+++ main_app/src/mainapp.cpp
@@ -38,37 +38,11 @@ extern "C"
 {
 #endif
 
-bool tlc5955_callback {false};
 
-#ifdef USE_TLC5955_HAL_DRIVER
-	#include <stm32g0xx_hal.h>
-	void HAL_SPI_TxCpltCallback(SPI_HandleTypeDef *hspi)
-	{
-		UNUSED(hspi);      
-		//HAL_SPI_DMAPause(&hspi2);
+tlc5955::Driver leds;
 
-		// disable the gsclk
-		HAL_GPIO_WritePin(TLC5955_SPI2_GSCLK_GPIO_Port, TLC5955_SPI2_GSCLK_Pin, GPIO_PIN_RESET);
-		
-		// toggle the latch pin
-		//HAL_Delay(1);
-		HAL_GPIO_WritePin(TLC5955_SPI2_LAT_GPIO_Port, TLC5955_SPI2_LAT_Pin, GPIO_PIN_SET);
-		//HAL_Delay(1);
-		HAL_GPIO_WritePin(TLC5955_SPI2_LAT_GPIO_Port, TLC5955_SPI2_LAT_Pin, GPIO_PIN_RESET);
-		//HAL_Delay(1);
-
-		// enable the gsclk
-		HAL_GPIO_WritePin(TLC5955_SPI2_GSCLK_GPIO_Port, TLC5955_SPI2_GSCLK_Pin, GPIO_PIN_SET);	
-		
-		//HAL_SPI_DMAResume(&hspi2);	
-	}    	
-#endif	
-
-void mainapp()
+void setup_leds()
 {
-
-	tlc5955::Driver leds;
-
 	// refresh buffers
 	leds.reset();
 	// construct the SPI transmit data
@@ -77,12 +51,45 @@ void mainapp()
 	leds.set_function_cmd(true, true, true, false, true);
 	leds.set_global_brightness_cmd(0x7F, 0x7F, 0x7F);
 	leds.set_max_current_cmd(0x5, 0x5, 0x5);
-	leds.set_dot_correction_cmd_all(0x01);
+	leds.set_dot_correction_cmd_all(0x7F);
 	// prepare SPI transmit data as bytes
 	leds.process_register();
 	// send the data
 	leds.toggle_latch(true);
 	leds.send_blocking_transmit();
+}
+
+void leds_on()
+{
+	// refresh buffers
+	leds.reset();
+	// construct the SPI transmit data
+	leds.set_greyscale_cmd_all(0x7FFF);
+	// prepare SPI transmit data as bytes
+	leds.process_register();
+	// send the data
+	leds.toggle_latch(false);
+	leds.send_blocking_transmit();
+}
+
+void leds_off()
+{
+	// refresh buffers
+	leds.reset();
+	// construct the SPI transmit data
+	leds.set_greyscale_cmd_all(0x0000);
+	// prepare SPI transmit data as bytes
+	leds.process_register();
+	// send the data
+	leds.toggle_latch(false);
+	leds.send_blocking_transmit();
+}
+
+void mainapp()
+{
+	// initial setup may fail during Power-On-Reset (POR) so do it twice
+	setup_leds();
+	setup_leds();
 
 	static ssd1306::Font5x7 font;
 	static ssd1306::Font16x26 font2;
@@ -93,7 +100,7 @@ void mainapp()
 
 	#ifdef USE_FULL_LL_DRIVER
 		// set the max freq (16MHz) for PWM when TIM4_ARR is also 1.
-		LL_TIM_OC_SetCompareCH1(TIM4, 0x1);
+		// LL_TIM_OC_SetCompareCH1(TIM4, 0x1);
 		// enable the timer but not the output
 		LL_TIM_EnableCounter(TIM4);
 	#endif    	
@@ -101,26 +108,8 @@ void mainapp()
 	//uint32_t delay_ms {0};
 	while(true)
 	{
-
-		// refresh buffers
-		leds.reset();
-		// construct the SPI transmit data
-		leds.set_greyscale_cmd_all(0x0000);
-		// prepare SPI transmit data as bytes
-		leds.process_register();
-		// send the data
-		leds.toggle_latch(false);
-		leds.send_blocking_transmit();
-	
-	// send the control data
-	// leds.send_blocking_transmit();
-
-		// // turn all LEDs off
-		// leds.reset();
-		// leds.set_latch_cmd(false);
-		// leds.set_greyscale_cmd_all(0x0000);
-		// leds.process_register();
-		// leds.send_blocking_transmit();
+		
+		leds_off();
 		
 		// run oled animation		
 		std::string msg {font2.character_map[count]};
@@ -133,22 +122,7 @@ void mainapp()
 			// LL_mDelay(100);
 		#endif
 
-		// refresh buffers
-		leds.reset();
-		// construct the SPI transmit data
-		leds.set_greyscale_cmd_all(0x7FFF);
-		// prepare SPI transmit data as bytes
-		leds.process_register();
-		// send the data
-		leds.toggle_latch(false);
-		leds.send_blocking_transmit();
-
-		// // turn all LEDs on
-		// leds.reset();
-		// leds.set_latch_cmd(false);
-		// leds.set_greyscale_cmd_all(0x7FFF);
-		// leds.process_register();
-		// leds.send_blocking_transmit();
+		leds_on();
 		
 		#ifdef USE_FULL_LL_DRIVER
 			embed_utils::tim::ll_delay_microsecond(TIM14, 0x01);
