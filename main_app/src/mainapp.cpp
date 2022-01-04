0954b5dda4300ac9f6fa123abac0c39be7511e4e
cracked-machine <chrisjsutton@gmail.com>
2022-01-04 18:31:46 +0000

    updated example code

--- main_app/src/mainapp.cpp
+++ main_app/src/mainapp.cpp
@@ -41,92 +41,117 @@ extern "C"
 
 tlc5955::Driver leds;
 
+// @brief Temp helper function to set the up leds object
 void setup_leds()
 {
 	// refresh buffers
 	leds.reset();
+	
 	// construct the SPI transmit data
 	leds.set_ctrl_cmd();
 	leds.set_padding_bits();
-	leds.set_function_cmd(true, true, true, false, true);
-	leds.set_global_brightness_cmd(0x7F, 0x7F, 0x7F);
-	leds.set_max_current_cmd(0x5, 0x5, 0x5);
+	leds.set_function_cmd(	
+		tlc5955::Driver::DisplayFunction::display_repeat_off,
+		tlc5955::Driver::TimingFunction::timing_reset_off,
+		tlc5955::Driver::RefreshFunction::auto_refresh_off,
+		tlc5955::Driver::PwmFunction::normal_pwm,
+		tlc5955::Driver::ShortDetectFunction::threshold_90_percent
+	);
+
+	// leds.set_function_cmd(true, false, false, false, true);
+	leds.set_global_brightness_cmd(0x1, 0x1, 0x1);
+	leds.set_max_current_cmd(0x1, 0x1, 0x1);
 	leds.set_dot_correction_cmd_all(0x7F);
+	
 	// prepare SPI transmit data as bytes
 	leds.process_register();
-	// send the data
-	leds.toggle_latch(true);
-	leds.send_blocking_transmit();
+
+	// send data for top row (no latch)
+	leds.send_first_bit(tlc5955::DataLatchType::control);
+	leds.send_spi_bytes(tlc5955::LatchPinOption::no_latch);
+	
+	// send data for bottom row
+	leds.send_first_bit(tlc5955::DataLatchType::control);
+	leds.send_spi_bytes(tlc5955::LatchPinOption::latch_after_send);
 }
 
-void leds_on()
+// @brief Temp helper function to turn on LEDs
+// @param pwm brightness value
+// @param led_position position: 0-15
+void leds_on(uint16_t pwm, uint16_t led_position)
 {
 	// refresh buffers
 	leds.reset();
-	// construct the SPI transmit data
-	leds.set_greyscale_cmd_all(0x7FFF);
+	
+	// set the colour - purple
+	leds.set_greyscale_cmd_rgb_at_position(led_position, pwm, 0, pwm);
+	
 	// prepare SPI transmit data as bytes
 	leds.process_register();
-	// send the data
-	leds.toggle_latch(false);
-	leds.send_blocking_transmit();
+	
+	// send data for top row (no latch)
+	leds.send_first_bit(tlc5955::DataLatchType::greyscale);
+	leds.send_spi_bytes(tlc5955::LatchPinOption::no_latch);
+
+	// send data for bottom row
+	leds.send_first_bit(tlc5955::DataLatchType::greyscale);
+	leds.send_spi_bytes(tlc5955::LatchPinOption::latch_after_send);	
 }
 
+// @brief Temp helper function to turn of LEDs
 void leds_off()
 {
 	// refresh buffers
 	leds.reset();
-	// construct the SPI transmit data
-	leds.set_greyscale_cmd_all(0x0000);
-	// prepare SPI transmit data as bytes
 	leds.process_register();
-	// send the data
-	leds.toggle_latch(false);
-	leds.send_blocking_transmit();
+
+	// send data for top row (no latch)
+	leds.send_first_bit(tlc5955::DataLatchType::greyscale);
+	leds.send_spi_bytes(tlc5955::LatchPinOption::no_latch);
+
+	// send data for bottom row	
+	leds.send_first_bit(tlc5955::DataLatchType::greyscale);
+	leds.send_spi_bytes(tlc5955::LatchPinOption::latch_after_send);	
+
 }
 
 void mainapp()
 {
-	// initial setup may fail during Power-On-Reset (POR) so do it twice
-	setup_leds();
+	// setup led driver
+	uint16_t pwm_counter = 0xFFFF;
+	uint16_t led_position = 0;
 	setup_leds();
 
+	// setup display driver
+	uint8_t font_count = 0;
 	static ssd1306::Font5x7 font;
 	static ssd1306::Font16x26 font2;
 	static ssd1306::Display oled;
-
 	oled.init();
-	uint8_t count = 0;
 
-	#ifdef USE_FULL_LL_DRIVER
-		// set the max freq (16MHz) for PWM when TIM4_ARR is also 1.
-		// LL_TIM_OC_SetCompareCH1(TIM4, 0x1);
-		// enable the timer but not the output
-		LL_TIM_EnableCounter(TIM4);
-	#endif    	
-
-	//uint32_t delay_ms {0};
 	while(true)
 	{
 		
-		leds_off();
+		leds_on(pwm_counter, led_position);		
+		(led_position >= 15) ? led_position = 0 : led_position++;
 		
 		// run oled animation		
-		std::string msg {font2.character_map[count]};
+		std::string msg {font2.character_map[font_count]};
 		oled.write(msg, font2, 2, 2, ssd1306::Colour::Black, ssd1306::Colour::White, 3, true);
-		if (count < font2.character_map.size() - 1) { count++; }
-		else { count = 0; }		
+		if (font_count < font2.character_map.size() - 1) { font_count++; }
+		else { font_count = 0; }		
 
 		#ifdef USE_FULL_LL_DRIVER
-			embed_utils::tim::ll_delay_microsecond(TIM14, 0x01);
-			// LL_mDelay(100);
+			//embed_utils::tim::ll_delay_microsecond(TIM14, 0x1);
+			LL_mDelay(200);
 		#endif
 
-		leds_on();
+		leds_off();
+
 		
 		#ifdef USE_FULL_LL_DRIVER
-			embed_utils::tim::ll_delay_microsecond(TIM14, 0x01);
-			// LL_mDelay(100);
+			// embed_utils::tim::ll_delay_microsecond(TIM14, 0xFFFE);
+			LL_mDelay(200);
 		#endif
 	}
 }
