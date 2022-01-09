726dbd07f19aa297cb7762f2c2d0ca8325ae6ac4
cracked-machine <chrisjsutton@gmail.com>
2022-01-09 23:36:04 +0000

    updated cube project to generate SPI1 DMA

--- stm32cube_workspace/G0B1KET6N/Core/Src/main.c
+++ stm32cube_workspace/G0B1KET6N/Core/Src/main.c
@@ -19,6 +19,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "dma.h"
 #include "i2c.h"
 #include "spi.h"
 #include "tim.h"
@@ -97,6 +98,7 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_DMA_Init();
   MX_SPI1_Init();
   MX_TIM14_Init();
   MX_TIM4_Init();
