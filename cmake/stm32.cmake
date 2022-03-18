63a91748af1631ed5b0d44576069489c10efbfcb
cracked-machine <chrisjsutton@gmail.com>
2022-03-18 04:46:17 +0000

    tidied up makefiles

--- cmake/stm32.cmake
+++ cmake/stm32.cmake
@@ -21,22 +21,23 @@ set(HEX_NAME            build.hex)
 
 # common build settings
 set(STACK_USAGE "-fstack-usage -Wstack-usage=10000")
+set(OPTIMZATIONS "-ffunction-sections -Wl,--gc-sections -fdata-sections")
 set(WARNING_FLAGS "-Wall -Werror -Wextra -Wdouble-promotion -Wformat=2 -Wformat-overflow -Wundef -Wformat-truncation -Wfloat-equal -Wshadow")
-set(COMMON_FLAGS "-march=${ARCH} -mcpu=${CORE} -${ARM_ASM} ${STACK_USAGE} ${WARNING_FLAGS} ${OPTIM_LVL} ${DEBUG_LVL} ${ADDITIONAL_FLAGS} -mfloat-abi=soft -pedantic -Wall -Wextra -Wfloat-equal -Wshadow -Wall -Wl,--gc-sections -fmessage-length=0 -ffunction-sections -fdata-sections -ffreestanding -fno-builtin")
-set(CMAKE_EXE_LINKER_FLAGS  "-mthumb -mcpu=cortex-m0 -specs=nosys.specs -static -Wl,-Map=build.map -Wl,--gc-sections -Wl,--defsym=malloc_getpagesize_P=0x80 -Wl,--start-group -lc -lm -Wl,--end-group -T${LINKER_SCRIPT}" CACHE INTERNAL "exe link flags")
+set(COMMON_FLAGS "-march=${ARCH} -mcpu=${CORE} -${ARM_ASM} -mfloat-abi=soft -ffreestanding -fno-builtin ")
+set(CMAKE_EXE_LINKER_FLAGS  "-mthumb -mcpu=${CORE} -specs=nosys.specs -T${LINKER_SCRIPT}" CACHE INTERNAL "exe link flags")
 
 # C compiler settings
-set(C_FLAGS "-ffunction-sections -fdata-sections -g3 -fstack-usage -Wno-unused-parameter -Wno-pedantic --specs=nano.specs")
-set(CMAKE_C_FLAGS           "${COMMON_FLAGS} ${C_FLAGS}" CACHE INTERNAL "c compiler flags")
+set(C_FLAGS "--specs=nano.specs")
+set(CMAKE_C_FLAGS "${C_FLAGS} ${COMMON_FLAGS} ${STACK_USAGE} ${WARNING_FLAGS} ${OPTIMZATIONS}" CACHE INTERNAL "c compiler flags")
 enable_language(C)
 
 # c++ compiler settings
-set(CPP_FLAGS "-fno-rtti -fno-exceptions -fno-use-cxa-atexit -fno-threadsafe-statics -ftemplate-backtrace-limit=0")
-set(CMAKE_CXX_FLAGS	        "${COMMON_FLAGS} ${CPP_FLAGS}" CACHE INTERNAL "cpp compiler flags")
+set(CPP_FLAGS "-fno-rtti -fno-exceptions -fno-use-cxa-atexit -fno-threadsafe-statics -ftemplate-backtrace-limit=0 ")
+set(CMAKE_CXX_FLAGS "${CPP_FLAGS} ${COMMON_FLAGS} ${STACK_USAGE} ${WARNING_FLAGS} ${OPTIMZATIONS}" CACHE INTERNAL "cpp compiler flags")
 enable_language(CXX)
 
 ## Assembler compiler settings
-set(CMAKE_ASM_FLAGS	        "${COMMON_FLAGS} -MP -MD -x assembler-with-cpp" CACHE INTERNAL "asm compiler flags")
+set(CMAKE_ASM_FLAGS "${COMMON_FLAGS} -MP -MD -x assembler-with-cpp" CACHE INTERNAL "asm compiler flags")
 enable_language(ASM)
 
 # Required to include STM32 LL framework
